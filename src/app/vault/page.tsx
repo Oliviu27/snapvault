ae339978af950e0d2d432b230e7141d24608731a
Oliviu27 <oliviu.neagu@zappistore.com>
2025-06-19 14:10:55 +0100

    main landing page

---
+++ src/app/vault/page.tsx
@@ -0,0 +1,10 @@
+
+export default function VaultPage() {
+  return (
+    <main className="min-h-screen bg-[#1b263b] p-6 text-white">
+      <h1 className="text-3xl mb-4">🏴‍☠️ SnapVault</h1>
+      MAKE IT PRETTY
+   
+    </main>
+  );
+}
